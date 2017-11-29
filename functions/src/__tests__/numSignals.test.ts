44e79888d0d99329788505d16e49a74bf21b15ec
Damien Stanton <damien.stanton@gmail.com>
2017-11-29 10:05:50 -0500

    tslint to tidy things up

--- functions/src/__tests__/numSignals.test.ts
+++ functions/src/__tests__/numSignals.test.ts
@@ -1,8 +1,16 @@
-import { fetchCount } from '../numSignals'
+import { fetchCount } from "../numSignals";
 
-const testData = '{ "TotalRows": 3, "ReturnedRows": 3, "Objects": [{ "key": 1, "count": 100 }, { "key": 2, "count": 200 }, { "key": 3, "count": 356 }], "Error": "" }'
+const testData = `{
+    "TotalRows": 3,
+    "ReturnedRows": 3,
+    "Objects": [{ "key": 1, "count": 100 },
+        { "key": 2, "count": 200 },
+        { "key": 3, "count": 356 }
+    ],
+    "Error": ""
+}`;
 
 test("Test parsing of counts from response body.", () => {
-    let total: Number = fetchCount(testData)
-    expect(total).toBe(656)
-})
+    const total: number = fetchCount(testData);
+    expect(total).toBe(656);
+});
--- functions/src/numSignals.ts
+++ functions/src/numSignals.ts
@@ -1,43 +1,44 @@
-import * as functions from "firebase-functions"
-import * as request from "request"
-
+import * as functions from "firebase-functions";
+import * as request from "request";
 
 /**
  * NumSignals reports the total number of signals indexed in the signal graph.
- * @param {Object} req GCF request context. 
- * @param {Object} res GCF response context. 
+ * @param {Object} req GCF request context.
+ * @param {Object} res GCF response context.
  */
 export let NumSignals = functions.https.onRequest((req: functions.Request, res: functions.Response) => {
-    const token: string = functions.config().floret.token
-    const url: string = functions.config().floret.url
-    const command: Object = JSON.stringify({ token: token, command: "signal-totals" })
-    const headers: Object = { "Content-Type": "application/json; charset=utf-8" }
+    const token: string = functions.config().floret.token;
+    const url: string = functions.config().floret.url;
+    const command: string = JSON.stringify({ token, command: "signal-totals" });
+    const headers: object = { "Content-Type": "application/json; charset=utf-8" };
     const postConfig = {
-        url: url,
-        headers: headers,
         body: command,
-    }
+        headers,
+        url,
+    };
 
     request.post(postConfig, (err, resp, content) => {
-        var total: Number = 0
-        if (!err && resp.statusCode == 200) {
-            total = fetchCount(content)
+        let total: number = 0;
+        if (!err && resp.statusCode === 200) {
+            total = fetchCount(content);
         }
-        let response: string = "There are currently " + total + " signals indexed. That's a lot!"
-        console.log("Sending response total to user: " + total)
-        res.setHeader("Content-Type", "application/json")
-        res.send(JSON.stringify({ "speech": response, "displayText": response }))
-    })
-})
+        const response: string = "There are currently " + total + " signals indexed. That's a lot!";
+        // console.log("Sending response total to user: " + total)
+        res.setHeader("Content-Type", "application/json");
+        res.send(JSON.stringify({ speech: response, displayText: response }));
+    });
+});
 
-export let fetchCount = (content): Number => {
-    let total: number = 0
-    const body = JSON.parse(content)
-    const counts = body.Objects
-    for (let i in counts) {
-        let data = counts[i]
-        total += data.count
+export let fetchCount = (content): number => {
+    let total: number = 0;
+    const body = JSON.parse(content);
+    const counts = body.Objects;
+    for (const i in counts) {
+        if (counts.hasOwnProperty(i)) {
+            const data = counts[i];
+            total += data.count;
+        }
     }
 
-    return total
-}
+    return total;
+};
